4f502d15ff48c5071dbc668447cc6a62157dcc22
Erika Ito <etito@wisc.edu>
2016-10-28 11:58:17 -0500

    Update Reservoir_app.R

--- Reservoir_app.R
+++ Reservoir_app.R
@@ -53,3 +53,33 @@ DeepDiveData[,"words"]<-gsub("\",\"","COMMASUB",DeepDiveData[,"words"])
 DeepDiveData[,"poses"]<-gsub("\",\"","COMMASUB",DeepDiveData[,"poses"])
 
 # Download dictionary of unit names from Macrostrat Database
+
+UnitsURL<-paste("https://dev.macrostrat.org/api/units?project_id=1&format=csv&response=long")
+GotURL<-getURL(UnitsURL)
+UnitsFrame<-read.csv(text=GotURL,header=TRUE)
+
+StratNamesURL<-paste("https://dev.macrostrat.org/api/defs/strat_names?all&format=csv")
+GotURL<-getURL(StratNamesURL)
+StratNamesFrame<-read.csv(text=GotURL,header=TRUE)
+
+# Get rid of columns from UnitsFrame that are also in StratNamesFrame so the merge function can run smoothly EXCEPT "strat_name_id"
+UnitsFrameNoOverlap<-UnitsFrame[,!(names(UnitsFrame) %in% names(StratNamesFrame))]
+UnitsFrame<-UnitsFrame[,c(colnames(UnitsFrameNoOverlap),"strat_name_id")]
+
+Units<-merge(x = StratNamesFrame, y = UnitsFrame, by = "strat_name_id", all.x = TRUE)
+Units<-Units[,c("strat_name_id","strat_name_long","strat_name","unit_id","unit_name","col_id","t_age","b_age","max_thick","min_thick","lith")]
+Units<-na.omit(Units)
+
+# Add a column of mean thickness
+Units$mean_thick<-apply(Units,1,function(row) mean(row["max_thick"]:row["min_thick"]))
+
+# Make two dictionaries: one of long strat names and one of short strat names
+
+# make a matrix of long and short units only. 
+LongShortUnits<-Units[,c("strat_name_long","strat_name")]
+# remove duplicate rows
+LongShortUnits<-unique(LongShortUnits)
+
+# Create dictionaries
+LongUnitDictionary<-as.character(LongShortUnits[,"strat_name_long"])
+ShortUnitDictionary<-as.character(LongShortUnits[,"strat_name"])
