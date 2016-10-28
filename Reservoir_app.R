2c8675f501bc5b1138928ae7ac07806c3f48a6a0
Erika Ito <etito@wisc.edu>
2016-10-28 12:11:51 -0500

    Update Reservoir_app.R

--- Reservoir_app.R
+++ Reservoir_app.R
@@ -83,3 +83,51 @@ LongShortUnits<-unique(LongShortUnits)
 # Create dictionaries
 LongUnitDictionary<-as.character(LongShortUnits[,"strat_name_long"])
 ShortUnitDictionary<-as.character(LongShortUnits[,"strat_name"])
+    
+# Remove commas from DeepDiveData
+CleanedWords<-gsub(","," ",DeepDiveData[,"words"])
+
+# Search for the long unit names in CleanedWords
+Start<-print(Sys.time())
+LongUnitHits<-parSapply(Cluster,LongUnitDictionary,function(x,y) grep(x,y,ignore.case=FALSE, perl = TRUE),CleanedWords)
+End<-print(Sys.time())
+
+# Create a matrix of unit hits locations with corresponding unit names
+LongUnitHitsLength<-pbsapply(LongUnitHits,length)
+LongUnitNames<-rep(names(LongUnitHits),times=LongUnitHitsLength)
+LongUnitLookUp<-cbind(LongUnitNames,unlist(LongUnitHits))
+# convert matrix to data frame
+LongUnitLookUp<-as.data.frame(LongUnitLookUp)
+# convert row match location (denotes location in CleanedWords) column to numerical data
+colnames(LongUnitLookUp)[2]<-"MatchLocation"
+LongUnitLookUp[,"MatchLocation"]<-as.numeric(as.character(LongUnitLookUp[,"MatchLocation"]))
+
+# Remove MatchLocation rows which appear more than once (remove sentences in which more than one unit occurs)
+AcceptableHits<-names(table(LongUnitLookUp))[which(table(LongUnitLookUp[,"MatchLocation"])==1)]
+LongHitTable<-subset(LongUnitLookUp,LongUnitLookUp[,"MatchLocation"]%in%AcceptableHits==TRUE)
+
+# Locate documents in which matches occurred for each respective strat_name_long
+
+# Unlist the row location data for each element(unit) in LongUnitHits
+MatchRowList<-lapply(LongUnitHits,function(x) unlist(x))
+# Extract docid data associated with each row in MatchRowList
+MatchDocList<-lapply(MatchRowList,function(x) DeepDiveData[x,"docid"]) 
+
+# Find sentences within documents that have long unit names (MatchDocList) that also contain the word "aquifer"
+# Note that this does not sub out spaces for commas, because we are doing a single word search    
+findPairs<-function(DeepDiveData,MatchDocList,ShortUnitHits,Word="aquifer") {
+    FinalVector<-vector("logical",length=length(MatchDocList))
+    names(FinalVector)<-names(MatchDocList)
+    progbar<-txtProgressBar(min=0,max=length(FinalVector),style=3)
+    for (i in 1:length(FinalVector)) {
+        Temp<-DeepDiveData[ShortUnitHits[[i]],]
+        DocSubset<-subset(Temp,Temp[,"docid"]%in%MatchDocList[[i]]==TRUE)
+        FinalVector[i]<-length(grep(Word,DocSubset[,"words"],ignore.case=TRUE))
+        setTxtProgressBar(progbar,i)
+        }
+    close(progbar)
+    return(FinalVector)
+    }
+    
+Matches<-findPairs(DeepDiveData[,c("docid","words")],MatchDocList,ShortUnitHits,"aquifer")    
+    
